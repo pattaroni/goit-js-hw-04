1fb292b982f7041e8f37ac7ae8362d6f81846851
Oleksiy Kyryk <105497306+pattaroni@users.noreply.github.com>
2025-05-29 11:27:52 +0300

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function makeArray(firstArray, secondArray, maxLength) {
+  let Array = firstArray.concat(secondArray);
+  let arr = [];
+  if (Array.length > maxLength) {
+    for (let i = 0; i < maxLength; i++) {
+      arr.push(Array[i]);
+    }
+    return arr;
+  } else {
+    return Array;
+  }
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
